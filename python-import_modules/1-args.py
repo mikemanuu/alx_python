74cebcc1335abf123f9e78878f5f77b0168f99c0
mikemanuu <mikemanuu273@gmail.com>
2023-07-24 09:37:17 -0700

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    
+if __name__ == "__main__":   
     import sys
-
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
