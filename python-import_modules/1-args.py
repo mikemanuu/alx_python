51940a74b4bedf8c6254743021bbff09991589e3
mikemanuu <mikemanuu273@gmail.com>
2023-07-25 05:06:17 -0700

    task 1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    args_number = len(argv) - 1
-    
-    if args_number > 0:
-        if args_number == 1:
-            print("{} argument:".format(args_number))
-        else:
-            print("{} arguments:".format(args_number))
-        i = 1
-        for args in argv[1:]:
-            print("{}: {}".format(i, args))
-            i += 1
-        else:
-            print("{} arguments:".format(args_number))
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
