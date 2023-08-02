8c754f34177aee345f5c964cd32c0dba7f04c4cd
mikemanuu <mikemanuu273@gmail.com>
2023-08-02 12:45:47 +0300

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+""" Module Square """
+
+
 class Square:
+
+    """
+    A square class that defines square attribute,
+    private instance attribute size
+    """
+
     def __init__(self, size):
         self.__size = size
