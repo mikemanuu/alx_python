3971dabdb1593125b81bdae2e3eba34e615c4847
mikemanuu <mikemanuu273@gmail.com>
2023-08-02 13:15:06 +0300

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module Square """
+
+
+class square:
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise('size must be an integer')
+
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return(self.__size * self.__size)
