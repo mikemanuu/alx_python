9d1862813c6be6ebb9a8ddc0f1eb78d46d5eadcf
mikemanuu <mikemanuu273@gmail.com>
2023-08-02 13:20:07 +0300

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,16 +4,24 @@
 
 class square:
 
+    """
+    A class Square that defines a square by:
+    Private instance attribute: size.
+
+    """
+
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+        ''' Retrieves private property size '''
 
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''property setter'''
 
         if not isinstance(value, int):
             raise('size must be an integer')
@@ -23,4 +31,6 @@ class square:
         self.__size = value
 
     def area(self):
+        ''' Returns the current area '''
+
         return(self.__size * self.__size)
