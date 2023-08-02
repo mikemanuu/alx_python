babebee2f8aa40fb7e7e05643e20e427b4a13d09
mikemanuu <mikemanuu273@gmail.com>
2023-08-02 13:37:45 +0300

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,39 +2,30 @@
 """ Module Square """
 
 
-class square:
-
-    """
-    A class Square that defines a square by:
-    Private instance attribute: size.
-    returns:
-    Private attribute size
-    """
+class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """ Constructor method for the Square class """
-        self.__size = size
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        ''' Retrieves private property size '''
-
-        return self.__size
+        """Get the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''property setter'''
-
-        '''check if size is integer'''
         if not isinstance(value, int):
-            raise('size must be an integer')
-
-        ''' check if size is greater than 0 '''
-        if value < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        ''' Returns the current area '''
-
-        return(self.__size * self.__size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
