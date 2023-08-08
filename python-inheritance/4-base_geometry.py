659b05fa3b1e2b235e860bbfa5ca26e5802a4e35
mikemanuu <mikemanuu273@gmail.com>
2023-08-08 11:48:04 +0300

    task 4

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
+    def __dir__(self):
+        return sorted(dir(type(self)) + list(self.__dict__) + ['area'])
