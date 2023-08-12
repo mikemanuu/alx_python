0d4e582acccfe1a41ec8edf306825affa6a140df
mikemanuu <mikemanuu273@gmail.com>
2023-08-12 12:13:46 +0300

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,17 +4,5 @@
 import sys
 import requests
 
-
-def main():
-    url = sys.argv[1]
-    r = requests.get(url)
-    if 'X-Request-Id' in r.headers:
-        r1 = r.headers['X-Requeast-Id']
-        print(r1)
-
-    else:
-        print("None")
-
-
-if __name__ == "__main__":
-    main()
+url = sys.argv[1]
+print(response.headers.get("X-Request-Id"))
