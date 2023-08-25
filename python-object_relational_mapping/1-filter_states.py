4278ae8e13ecac31e9380af5a40e4b6906a879a1
mikemanuu <mikemanuu273@gmail.com>
2023-08-25 17:46:07 +0300

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""  List all states with a name starting with N (upper N) from the database hbtn_0e_0_usa. """
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELCT * FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+
+    cur.close()
+    db.close()
