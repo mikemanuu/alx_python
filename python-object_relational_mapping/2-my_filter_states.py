580c1946c9fe52c523c40fe01f1688fe3682acce
mikemanuu <mikemanuu273@gmail.com>
2023-08-25 18:05:02 +0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
+
+import sys
+import MySQLdb
+
+if __name__ == '__main_-':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELEC")
