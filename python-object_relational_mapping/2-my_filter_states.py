0e72d639d265c4bc0b379bbda83315c470aa8328
mikemanuu <mikemanuu273@gmail.com>
2023-08-25 18:06:18 +0300

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,4 +8,11 @@ if __name__ == '__main_-':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELEC")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE '{:S}' ORDER BY \ id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
