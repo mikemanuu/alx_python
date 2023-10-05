2c6e4e28673d4a20e2edac3c8adb0fbb494a6f10
mikemanuu <mikemanuu273@gmail.com>
2023-10-05 18:08:30 +0300

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,9 +21,13 @@ def fetch_employee_data(employee_id):
 
 
 def fetch_todo_list(employee_id):
+    """ Define base url for api """
     url = "https://jsonplaceholder.typicode.com"
+
+    """ GET request to retrieve employee information. """
     response = requests.get(f"{url}/users/{employee_id}/todos")
 
+    """ Check if request was successful. """
     if response.status_code == 200:
         todo_list = response.json()
         return todo_list
@@ -39,15 +43,24 @@ def main():
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
+
+    # Fetch employee data
     employee_data = fetch_employee_data(employee_id)
 
     if employee_data:
+
+        # Fetch todo list for employee
         todo_list = fetch_todo_list(employee_id)
         if todo_list:
+
+            # Calculate no of completed tasks
             completed_tasks = [task for task in todo_list if task["completed"]]
             num_completed_tasks = len(completed_tasks)
+
+            # calculate total no of tasks
             total_tasks = len(todo_list)
 
+            # Display employee's information and task progress
             print(
                 f"Employee {employee_data['name']} is done with tasks({num_completed_tasks}/{total_tasks}):")
             for task in completed_tasks:
