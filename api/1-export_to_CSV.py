00b430011d8dcc6246c3c7bf1ee25f342f122306
mikemanuu <mikemanuu273@gmail.com>
2023-10-10 15:13:57 +0300

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,45 +5,25 @@ import csv
 import requests
 import sys
 
-# Not executed
 if __name__ == "__main__":
+    user_id = sys.argv[1]
 
-    # Pass employee id on command line
-    id = sys.argv[1]
+    # getting user info
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    user = requests.get(user_url)
+    user_data = user.json()
 
-# API
-    userTodoURL = "https://jsonplaceholder.typicode.com/users/1/todos".format(
-        id)
-    userProfile = "https://jsonplaceholder.typicode.com/users/1".format(id)
+    # getting todo info
 
-# APIs requests
-    todoResponse = requests.get(userTodoURL)
-    profileResponse = requests.get(userProfile)
+    todos_url = 'https://jsonplaceholder.typicode.com/users/1/todos'.format(
+        user_id)
+    todos = requests.get(todos_url)
+    todos_data = todos.json()
 
-# Parse responses
-    todoJson_Data = todoResponse.json()
-    profileJson_Data = profileResponse.json()
-
-# Get employee information
-    employeeName = profileJson_Data['username']
-
-    dataList = []
-
-    for data in todoJson_Data:
-        dataDict = {"userId": data['userId'], "name": employeeName,
-                    "completed": data['completed'], "title": data['title']}
-        dataList.append(dataDict)
-
-# Specify csv filepath
-    csv_file_path = '{}.csv'.format(todoJson_Data[0]['userId'])
-
-# Define the fieldnames
-    fieldnames = ["userId", "name", "completed", "title"]
-
-# Open csvfile in write mode
-    with open(csv_file_path, 'w', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-# Write data rows
-        for row in dataList:
-            csv_writer.writerow(row)
+    # Export data to csv
+    csv_file = "{}.csv".format(user_id)
+    with open(csv_file, 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for task in todos_data:
+            csv_writer.writerow(
+                [user_id, str(user_data['name']), task['completed'], task['title']])
