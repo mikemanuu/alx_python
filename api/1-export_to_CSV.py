991a008175c537bb0f3113065702c2405b88fe8e
mikemanuu <mikemanuu273@gmail.com>
2023-10-10 09:23:46 +0300

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,14 +6,34 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="\n") as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, t.get(
-            "completed"), t.get("title")]) for t in todos]
-__doc__ = """doc for module"""
+    id = sys.argv[1]
+
+    userTodoURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        id)
+    userProfile = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+
+    todoResponse = requests.get(userTodoURL)
+    profileResponse = requests.get(userProfile)
+
+    todoJson_Data = todoResponse.json()
+    profileJson_Data = profileResponse.json()
+
+    employeeName = profileJson_Data['username']
+
+    dataList = []
+
+    for data in todoJson_Data:
+        dataDict = {"userId": data['userId'], "name": employeeName,
+                    "completed": data['completed'], "title": data['title']}
+        dataList.append(dataDict)
+
+    csv_file_path = '{}.csv'.format(todoJson_Data[0]['userId'])
+
+    fieldnames = ["userId", "name", "completed", "title"]
+
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        for row in dataList:
+            csv_writer.writerow(row)
