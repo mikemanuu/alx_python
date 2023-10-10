ce23f2899fa6af002e4cc50961316a9a2967e5fa
mikemanuu <mikemanuu273@gmail.com>
2023-10-10 11:15:29 +0300

    task 4

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -2,8 +2,4 @@
 BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
-
-try:
-    print(bg.area())
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+print(dir(bg))
