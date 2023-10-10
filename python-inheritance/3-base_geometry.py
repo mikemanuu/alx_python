4c4fdcde5edbbd2368d315b5f1e41eb8a32c7249
mikemanuu <mikemanuu273@gmail.com>
2023-10-10 11:02:15 +0300

    task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,14 +2,16 @@
 """Defines an empty class BaseGeometry."""
 
 
-class BaseGeometry:
-    """Represent base geometry."""
+class BaseGeometryMetaClass(type):
+    """ Class to override the init subclass in base geometry class. """
+    def __dir__(cls):
+        attributes = super().__dir__()
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
 
-    def __init_subclass__(cls):
-        pass
 
-    def __dir__(self):
-        return[attr for attr in dir(type(self)) if attr != '__init_subclass__']
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """Creates empty class."""
 
-    def __class__(self):
-        return type(self)
+    def __dir__(cls):
+        attributes = super().__dir__()
+        return [attributes for attributes in attributes if attributes != '__init_subclass__']
