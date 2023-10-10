eec3fa32419ac92dd2b528ecba3b49ee5ab6270f
mikemanuu <mikemanuu273@gmail.com>
2023-10-10 11:40:05 +0300

    task 5

--- python-inheritance/5-main.py
+++ python-inheritance/5-main.py
@@ -2,4 +2,21 @@
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
-print(dir(bg))
+
+bg.integer_validator("my_init", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
