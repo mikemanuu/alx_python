d8f451264f57f5d03ef1f1c180424608f90ba1e4
mikemanuu <mikemanuu273@gmail.com>
2023-10-12 00:44:31 +0300

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,47 @@
 #!/usr/bin/python3
 """ Takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
-import sys
 import MySQLdb
+import sys
+
+
+def search_states(mysql_user, mysql_password, db_name, state_name):
+    try:
+        # Connect to the MySQL server
+        conn = MySQLdb.connect(
+            user=mysql_user,
+            passwd=mysql_password,
+            host='localhost',
+            port=3306,
+            db=db_name
+        )
+
+        # Create a cursor
+        cursor = conn.cursor()
+
+        # Prepare and execute the SQL query
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the matching rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print("MySQLdb Error:", e)
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY  id ASC".format(sys.argv[4]))
-    [print(state) for state in cur.fetchall()]
+    if len(sys.argv) != 5:
+        print(
+            "Usage: python script.py <mysql_user> <mysql_password> <db_name> <state_name>")
+    else:
+        mysql_user, mysql_password, db_name, state_name = sys.argv[1:5]
+        search_states(mysql_user, mysql_password, db_name, state_name)
