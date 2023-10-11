debf944a3f3cb9d1f8ef3ff8e4b986e666cbb4a4
mikemanuu <mikemanuu273@gmail.com>
2023-10-11 22:31:58 +0300

    task 7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
+
 """Defines a class Rectangle that inherits from BaseGeometry."""
+
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
-class CustomDirMeta(type):
-    def __dir__(cls):
-        desired_attributes = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-                              '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator']
-        return desired_attributes
-
-
-class Rectangle(BaseGeometry, metaclass=CustomDirMeta):
-    """ Represent a rectangle using Base Geometry. """
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
         """Intialize a new Rectangle.
@@ -26,11 +21,11 @@ class Rectangle(BaseGeometry, metaclass=CustomDirMeta):
         self.__height = height
 
     def area(self):
-        """Calculates the area of the rectangle"""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """ Returns the print() and str() representation of the rectangle. """
-        string = "[" + str(self.__class__.__name__) + "]"
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
