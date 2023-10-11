2fcd4ecb2c2fbf39572bccca6fd900bd2c7bc4aa
mikemanuu <mikemanuu273@gmail.com>
2023-10-11 21:28:20 +0300

    task 8

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,41 +5,45 @@ import csv
 import requests
 import sys
 
-
-def get_employee_todo_progress(employee_id):
-    # Get employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response_employee = requests.get(employee_url)
-    employee_data = response_employee.json()
-    user_id = employee_data.get("id")
-    username = employee_data.get("username")
-
-    # Get employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    response_todo = requests.get(todo_url)
-    todo_list = response_todo.json()
-
-    # Create a CSV file
-    csv_file_name = f"{user_id}.csv"
-    with open(csv_file_name, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        # Write TODO list data to CSV
-        for task in todo_list:
-            task_completed_status = "Completed" if task["completed"] else "Not Completed"
-            csv_writer.writerow(
-                [user_id, username, task_completed_status, task["title"]])
-
-    print(f"Data exported to {csv_file_name}")
-
-
+# Not executed
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
 
-get_employee_todo_progress(1)
+    # Pass employee id on command line
+    id = sys.argv[1]
+
+# API
+    userTodoURL = "https://jsonplaceholder.typicode.com/users/1/todos".format(
+        id)
+    userProfile = "https://jsonplaceholder.typicode.com/users/1".format(id)
+
+# APIs requests
+    todoResponse = requests.get(userTodoURL)
+    profileResponse = requests.get(userProfile)
+
+# Parse responses
+    todoJson_Data = todoResponse.json()
+    profileJson_Data = profileResponse.json()
+
+# Get employee information
+    employeeName = profileJson_Data['username']
+
+    dataList = []
+
+    for data in todoJson_Data:
+        dataDict = {"userId": data['userId'], "name": employeeName,
+                    "completed": data['completed'], "title": data['title']}
+        dataList.append(dataDict)
+
+# Specify csv filepath
+    csv_file_path = '{}.csv'.format(todoJson_Data[0]['userId'])
+
+# Define the fieldnames
+    fieldnames = ["userId", "name", "completed", "title"]
+
+# Open csvfile in write mode
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+# Write data rows
+        for row in dataList:
+            csv_writer.writerow(row)
---
+++ python-inheritance/main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+Square = __import__('8-square').Square
+
+print(dir(Square))
