c6e7e4bbb2d35885dc8d441837441aa290043771
mikemanuu <mikemanuu273@gmail.com>
2023-10-11 21:48:20 +0300

    task 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -20,7 +20,16 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer".format(name))
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def __dir__(self):
+        """ Include only the desired attributes. """
+        desired_attributes = ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+                              '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator']
+        return [attribute for attribute in desired_attributes if attribute != '__init_subclass__']
+
+    def __init_subclass__(cls):
+        pass
