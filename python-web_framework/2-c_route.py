c4babb8776c43200a7e100a48bb64c49cfaecc86
mikemanuu <mikemanuu273@gmail.com>
2023-10-11 23:29:49 +0300

    task 2

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """ starts a Flask web application and displays 'Hello HBNB', 'HBNB' and 'C' """
-
 from flask import Flask
+
 app = Flask(__name__)
 
 # this route is called by default when you run it
 
 
-@app.route("/", strict_slashes=False)
-def display():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     # displays message fto the user
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def show_message():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     # display HBNB message to the user
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def show_C(text):
+@app.route('/c/<text>', strict_slashes=False)
+def display_custom_text(text):
     # display C with text to the user
-    return "C {}".fromat(text.replace("_", " "))
+    return f"C {text}"
 
 
 if __name__ == "__main__":
