9fdeb6cbebc975a8feeeac1c3d15f7bc7e83a6d4
mikemanuu <mikemanuu273@gmail.com>
2023-10-11 22:46:10 +0300

    task 7

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
 Rectangle = __import__('7-rectangle').Rectangle
 
-try:
-    r = Rectangle()
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+r = Rectangle(5, 5)
+print(r)
 
-try:
-    r = Rectangle(1)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+r = Rectangle(1, 4)
+print(r)
 
-try:
-    r = Rectangle(1, [12, 52])
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r = Rectangle(4, 5)
-    print(r.width)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r = Rectangle(4, 5)
-    print(r.height)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+r = Rectangle(1411, 781)
+print(r)
