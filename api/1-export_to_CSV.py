83ec21ae3b261cce11b04bde7d43abaeed28d077
mikemanuu <mikemanuu273@gmail.com>
2023-10-12 12:13:07 +0300

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,66 +1,50 @@
 #!/usr/bin/python3
-""" Exports data in the CSV format. """
 import csv
-import logging
 import requests
 import sys
 
-# Configure logging
-logging.basicConfig(filename='export_data.log', level=logging.INFO)
 
+def get_employee_data(employee_id):
+    # Define the API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-def user_info(employee_id):
-    # Fetch employee information
     try:
-        employee_response = requests.get(
-            f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-        employee_response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching employee data: {e}")
-        print("Error: Unable to fetch employee data.")
-        return
+        # Fetch employee data
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        user_id = user_data.get('id')
+        username = user_data.get('username')
 
-    employee_data = employee_response.json()
-    employee_name = employee_data.get("name", "Unknown Employee")
-    employee_username = employee_data.get("username", "Unknown Username")
+        # Fetch TODO list data
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
 
-    # Fetch employee's tasks
-    try:
-        todos_response = requests.get(
-            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-        todos_response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        logging.error(
-            f"Error fetching tasks for employee ID {employee_id}: {e}")
-        print(f"Error: Unable to fetch tasks for employee ID {employee_id}.")
-        return
+        # Create a CSV file for the employee
+        csv_filename = f"{user_id}.csv"
+        with open(csv_filename, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
 
-    todo_data = todos_response.json()
-
-    # Create CSV
-    csv_filename = f"{employee_id}.csv"
-
-    with open(csv_filename, mode='w', newline="", encoding='utf-8') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        for task in todo_data:
-            task_completed_status = "Completed" if task["completed"] else "Not Completed"
+            # Write header row
             csv_writer.writerow(
-                [employee_id, employee_username, task_completed_status, task["title"]])
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    print(f"CSV file '{csv_filename}' created successfully.")
+            # Write task data to the CSV
+            for task in todos_data:
+                csv_writer.writerow(
+                    [user_id, username, task["completed"], task["title"]])
 
+        print(f"Data exported to {csv_filename}")
 
-if __name__ == "__main":
-    if len(sys.argv) != 2:
-        print("Usage: python3 main_o.py <employee_id>")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-        user_info(employee_id)
-    except ValueError:
-        print("Error: Employee ID must be an integer.")
-        logging.error("Error: Employee ID must be an integer.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+employee_id = int(sys.argv[1])
+get_employee_data(1)
