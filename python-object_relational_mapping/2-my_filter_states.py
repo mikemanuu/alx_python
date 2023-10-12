df135a36516005184fa2a7a7b18ee5ac18bc5e89
mikemanuu <mikemanuu273@gmail.com>
2023-10-12 10:22:48 +0300

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """Displays all values in the states table of
-hbtn_0e_0_usa."""
+hbtn_0e_0_usa where name matches the argument."""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3]
+    )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY  id ASC".format(sys.argv[4]))
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+        .format(sys.argv[4])
+    )
     [print(state) for state in cur.fetchall()]
